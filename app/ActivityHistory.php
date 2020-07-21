1c3118349d561668a5365be54a9a1e92c983f38e
Salih <salih_borucu@hotmail.com>
2020-07-21 17:58:14 +0100

    Activity History Create method refactored

--- app/ActivityHistory.php
+++ app/ActivityHistory.php
@@ -12,6 +12,74 @@ class ActivityHistory extends Model
 {
     protected $guarded = [];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($activityHistory) {
+            $activityHistory->adjustLeadAccountRecord($activityHistory);
+        });
+    }
+
+    public function adjustLeadAccountRecord($activityHistory)
+    {
+        $outcome = Outcome::find(request('outcome'));
+        $nextSchedule = $outcome->new_schedule_id ? $outcome->new_schedule_id :  request('lead.schedule_id');
+        $previousScheduleId = request('lead.schedule_id');
+        $previousStepNumber = request('lead.step.step_number');
+
+        if ($outcome->new_schedule_id) {
+            $nextStep = Steps::where('schedule_id', $outcome->new_schedule_id)
+                ->where('step_number', 1)
+                ->first();
+            $nextDueDate = request('lead.due_date');
+            $nextStatus = $outcome->name;
+
+            //if new schedule is custom
+            if (request('outcome') === "3") {
+                $nextDueDate = request('custom_activity_date');
+                $nextStep = Steps::where('schedule_id', $outcome->new_schedule_id)
+                    ->where('step_number', request('custom_activity_type'))
+                    ->first();
+            }
+        } else {
+            $lastSchedule = request('lead.schedule_id');
+            $lastStepNumber = request('lead.step.step_number');
+
+            //custom previous schedule
+            if (request('lead.schedule_id') === 8) {
+                $lastSchedule = request('lead.previous_schedule_id');
+                $lastStepNumber = request('lead.previous_step_number');
+            }
+
+            $nextStep = Steps::where('schedule_id', $lastSchedule)
+                ->where('step_number', $lastStepNumber + 1)
+                ->first();
+
+            //if completed schedule
+            if (!$nextStep) {
+                $nextStep = 1;
+                $nextSchedule = 9;
+            }
+
+            $day_gap = request('lead.step.day_gap');
+            $nextDueDate = date('Y-m-d', strtotime(request('lead.due_date') . " +{$day_gap} day"));
+            $nextStatus = 'prospecting';
+        }
+
+        $activityHistory->leadAccount()->updateOrCreate([
+            'lead_id' => request('lead')['id'],
+            'account_id' => request('lead')['account_id'],
+            'campaign_id' => request('lead')['campaign_id'],
+            'schedule_id' => $nextSchedule,
+            'step_id' => $nextStep->id,
+            'due_date' => $nextDueDate,
+            'current_status' => $nextStatus,
+            'previous_step_number' => $previousStepNumber,
+            'previous_schedule_id' => $previousScheduleId,
+        ]);
+    }
+
     public function outcome()
     {
         return $this->belongsTo(Outcome::class);
--- app/Http/Controllers/ActivitiesController.php
+++ app/Http/Controllers/ActivitiesController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Steps;
 use App\Account;
-use App\Outcome;
 use App\Campaign;
 use Carbon\Carbon;
 use App\LeadAccount;
@@ -62,59 +60,6 @@ class ActivitiesController extends Controller
             'type' => request('lead.step.type'),
             'notes' => request('notes'),
             'created_at' => Carbon::now()->format('Y-m-d')
-        ]);
-
-        $outcome = Outcome::find(request('outcome'));
-        $nextSchedule = $outcome->new_schedule_id ? $outcome->new_schedule_id :  request('lead.schedule_id');
-        $previousScheduleId = request('lead.schedule_id');
-        $previousStepNumber = request('lead.step.step_number');
-
-        if ($outcome->new_schedule_id) {
-            $nextStep = Steps::where('schedule_id', $outcome->new_schedule_id)
-                ->where('step_number', 1)
-                ->first();
-            $nextDueDate = request('lead.due_date');
-            $nextStatus = $outcome->type;
-
-            //if new schedule is custom
-            if (request('outcome') === "3") {
-                $nextDueDate = request('custom_activity_date');
-                $nextStep = Steps::where('schedule_id', $outcome->new_schedule_id)
-                    ->where('step_number', request('custom_activity_type'))
-                    ->first();
-            }
-        } else {
-            $lastSchedule = request('lead.schedule_id');
-            $lastStepNumber = request('lead.step.step_number');
-
-            //custom previous schedule
-            if (request('lead.schedule_id') === 8) {
-                $lastSchedule = request('lead.previous_schedule_id');
-                $lastStepNumber = request('lead.previous_step_number');
-            }
-
-            $nextStep = Steps::where('schedule_id', $lastSchedule)
-                ->where('step_number', $lastStepNumber + 1)
-                ->first();
-
-            //if completed schedule
-            if (!$nextStep) {
-                $nextStep = 1;
-                $nextSchedule = 9;
-            }
-
-            $day_gap = request('lead.step.day_gap');
-            $nextDueDate = date('Y-m-d', strtotime(request('lead.due_date') . " +{$day_gap} day"));
-            $nextStatus = 'prospecting';
-        }
-
-        LeadAccount::find(request('lead.id'))->update([
-            'schedule_id' => $nextSchedule,
-            'step_id' => $nextStep->id,
-            'due_date' => $nextDueDate,
-            'current_status' => $nextStatus,
-            'previous_step_number' => $previousStepNumber,
-            'previous_schedule_id' => $previousScheduleId,
         ]);
     }
 }
