9bb83e39b49661cf34036e1c73f4c17f26fce3b7
Salih <salih_borucu@hotmail.com>
2020-07-25 23:53:54 +0100

    Added auth requirement for API points

--- app/Http/Controllers/IncompleteLeadController.php
+++ app/Http/Controllers/IncompleteLeadController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Company;
 use App\IncompleteLead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IncompleteLeadController extends Controller
 {
@@ -18,22 +19,23 @@ class IncompleteLeadController extends Controller
 
     public function create()
     {
-        // dd(json_decode(request()->getContent(), true)['first_name']);
         $request = json_decode(request()->getContent(), true);
-        dd($request['user_email']);
 
-        IncompleteLead::create([
-            'first_name' => $request['first_name'],
-            'last_name' => $request['last_name'],
-            'company_name' => $request['company_name'],
-            'country' => $request['country'],
-            'title' => $request['title'],
-            'linkedin' => $request['linkedin'],
-            'user_id' => 1,
-            'unassigned' => true
-        ]);
+        if (Auth::attempt(['email' => $request['userEmail'], 'password' => $request['userPassword']])) {
+            IncompleteLead::create([
+                'first_name' => $request['first_name'],
+                'last_name' => $request['last_name'],
+                'company_name' => $request['company_name'],
+                'country' => $request['country'],
+                'title' => $request['title'],
+                'linkedin' => $request['linkedin'],
+                'user_id' => 1,
+                'unassigned' => true
+            ]);
+            return response('done');
+        }
 
-        return response('done');
+        return response('Wrong login data.');
     }
 
     public function destroy(IncompleteLead $incompleteLead)
